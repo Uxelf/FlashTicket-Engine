a6dc6f9713b8fc062a863e1ecd443ac694be3575
Uxelf <uxself@gmail.com>
2026-07-08 16:26:46 +0200

    feat: initialize configured seats

---
+++ backend/src/main/java/com/uxelf/dev/ticketDealer/repository/SeatRepository.java
@@ -0,0 +1,13 @@
+package com.uxelf.dev.ticketDealer.repository;
+
+import com.uxelf.dev.ticketDealer.entity.Seat;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface SeatRepository extends JpaRepository<Seat, UUID> {
+    List<Seat> findAllByRoomId(UUID id);
+}
